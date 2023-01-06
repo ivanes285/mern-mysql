cc358423cace37df99a79d9a27d181027c8b8653
ivanes285 <ivanes285@gmail.com>
2023-01-06 15:28:45 -0500

    Fix css

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="bg-slate-800  flex justify-between px-20 py-4 text-xl items-center flex-wrap ">
-     <Link to="/"><h1 className="text-2xl font-bold  ">React MySQL</h1></Link>
+    <div className="bg-slate-800 flex justify-between px-20 py-4 text-xl items-center flex-wrap ">
+     <Link to="/"><h1 className="text-2xl font-bold">React MySQL</h1></Link>
       <nav className=" gap-6 flex flex-row flex-wrap">
         <Link className=" hover:border-b border-red-500 " to="/">Home</Link>
         <Link className=" hover:border-b border-red-500 " to="/newtask">NewTask</Link>
